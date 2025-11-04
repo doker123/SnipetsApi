6dac361747399a9226dfb32ad6649c4256d9b1d8
Doker123 <dodo2larin@gmail.com>
2025-11-04 16:47:11 +0700

    Доделал api snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,2 +1,29 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions, renderers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Snippet
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
